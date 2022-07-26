be5d57289d8d6814bf47cc7dd86a06f0c7f69245
macm <mac.mitsch@comcast.net>
2022-07-26 07:54:36 -0500

    homepage is done

--- controllers/index.js
+++ controllers/index.js
@@ -3,7 +3,9 @@ const router = require('express').Router();
 const userRoutes = require('./api/user-routes');
 const postRoutes = require('./api/post-routes');
 const commentRoutes = require('./api/comment-routes');
+const homeRoutes = require('./home-routes');
 
+router.use('/',homeRoutes);
 router.use('/users', userRoutes);
 router.use('/posts', postRoutes);
 router.use('/comments', commentRoutes);
